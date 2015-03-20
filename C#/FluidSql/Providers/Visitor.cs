1dc52e50978e7a978e88a91840562ee5d14de4f9
ttrider <vyangurs@hotmail.com>
2015-03-20 15:24:03 -0700

    adding missing file

---
+++ C#/FluidSql/Providers/Visitor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTRider.FluidSql.Providers
+{
+    public class Visitor
+    {
+
+
+        protected class Sym
+        {
+            public const string _ALL_ = " ALL ";
+            public const string _AND_ = " AND ";
+            public const string _ANY_ = " ANY ";
+            public const string _AS_ = " AS ";
+            public const string _AS_osp = " AS [";
+            public const string _ASC = " ASC";
+            public const string _BETWEEN_ = " BETWEEN ";
+            public const string _cp = " )";
+            public const string _DESC = " DESC";
+            public const string _EXISTS_ = " EXISTS ";
+            public const string _FROM_ = " FROM ";
+            public const string _GROUP_BY_ = " GROUP BY ";
+            public const string _HAVING_ = " HAVING ";
+            public const string _IDENTITY_ = " IDENTITY ";
+            public const string _IN_op = " IN (";
+            public const string _INTO_ = " INTO ";
+            public const string _IS_NOT_NULL = " IS NOT NULL";
+            public const string _IS_NULL = " IS NULL";
+            public const string _LIKE_ = " LIKE ";
+            public const string _MAXDOP = " MAXDOP";
+            public const string _NOT_IN_op = " NOT IN (";
+            public const string _NOT_op = " NOT (";
+            public const string _ON_ = " ON ";
+            public const string _ONLINE = " ONLINE";
+            public const string _op = " (";
+            public const string _ORDER_BY_ = " ORDER BY ";
+            public const string _osp = " [";
+            public const string _OUTPUT_ = " OUTPUT ";
+            public const string _PERCENT = " PERCENT";
+            public const string _TOP_op = " TOP (";
+            public const string _WHERE_ = " WHERE ";
+            public const string _WITH_ = " WITH ";
+            public const string _WITH = " WITH";
+            public const string _WITH_TIES = " WITH TIES";
+            public const string ALTER_VIEW_ = "ALTER VIEW ";
+            public const string COMMA_ = ", ";
+            public const string cp = ")";
+            public const string cpsc = ");";
+            public const string CREATE_VIEW_ = "CREATE VIEW ";
+            public const string csp = "]";
+            public const string DELETE = "DELETE";
+            public const string DROP_INDEX_ = "DROP INDEX ";
+            public const string DROP_VIEW_ = "DROP VIEW ";
+            public const string NULL = "NULL";
+            public const string OFF = "OFF";
+            public const string ON = "ON";
+            public const string op = "(";
+            public const string osp = "[";
+            public const string sc = ";";
+            public const string SPACE = " ";
+            
+        }
+    }
+}
