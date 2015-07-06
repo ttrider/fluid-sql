eaa4e60680020ef2e0f0448f5be0533dee5dba4c
Vladimir Yangurskiy <vyangurs@hotmail.com>
2015-07-06 14:27:28 -0700

    update name on a license

--- C#/Shared/SharedAssemblyInfo.cs
+++ C#/Shared/SharedAssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]  
