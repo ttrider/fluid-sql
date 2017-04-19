f629339f0ff1f801d64875e7196a538a6865cff2
Vladimir Yangurskiy <software@ttrider.com>
2017-04-18 17:09:19 -0700

    copyright update

--- C#/TTRider.FluidSQL.Postgres/Providers/PostgreSQL/PostgreSQLVisitor.cs
+++ C#/TTRider.FluidSQL.Postgres/Providers/PostgreSQL/PostgreSQLVisitor.cs
@@ -1,7 +1,7 @@
 ﻿// <license>
 // The MIT License (MIT)
 // </license>
-// <copyright company="TTRider, L.L.C.">
+// <copyright company="TTRider Technologies, Inc.">
 // Copyright (c) 2014-2015 All Rights Reserved
 // </copyright>
 using System;
